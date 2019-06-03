cab6c55d2e0bd629f9ba882b915ccd4b43fcbbd3
Alicia Fasciocco <afasciocco@gmail.com>
2019-06-03 19:39:47 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,8 +1,8 @@
 def my_collect(collection)
   i = 0 
-  new_collection = []
+  new_coll = []
   while i < collection.size do
-    collection2 << yielf(collection[i])
+    new_coll << yield(collection[i])
     i+=1
   
 
